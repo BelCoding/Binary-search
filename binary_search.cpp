db55f2692710828a00d7187268c73ee66684b7bd
Bel <46499802+BelCoding@users.noreply.github.com>
2020-01-04 19:23:16 +0100

    Fix comments and parameters

--- binary_search.cpp
+++ binary_search.cpp
@@ -32,30 +32,25 @@ if( window_length == 0 )
 	{
 
 		window_length = window_length/2; // new window length
-		// Next position (left of the window)
-		// pos_left_w is tha same than before
+		// Next position (left of the window): pos_left_w wont change because of the number is on the left side
 
-		//std::cout << rest << "[rest]. Go to left, " << v[pos_left_w] << "["<<pos_left_w<<"], with input window length: " << window_length << std::endl;
-		return binary_search_research(v, thing2search, window_length, pos_left_w, it_count);
+		return binary_search_research(v, thing2search, window_length, pos_left_w);
 
-	}else if (thing2search > v[midpos]){ // It is on the rigth side:
+	}else if (thing2search > v[midpos]){ // It is on the right side:
 
-		// when window_length is even number, the window to the right side, must be 1 less position
-		window_length = (window_length-1)/2;
+		// if window_length is an even number, the window_length to the right side must be 1 item shorter
+		// this is because our midpos is not actually in the middle (not possible for even lengths), so when moving to right we have one less item.
+		window_length = (window_length - 1)/2; // think about integers division, to see that this -1 in the operation does not change anything for odd numbers.
 
 		// Next position (left of the window)
 		pos_left_w = midpos+1;
-
-		//std::cout << rest << "[rest]. Go to right, " << v[pos_left_w] << "["<<pos_left_w<<"], with input window length: " << window_length << std::endl;
-		return binary_search_research(v, thing2search, window_length, pos_left_w, it_count);
+		return binary_search_research(v, thing2search, window_length, pos_left_w);
 
 	}else{
 
-		pos_left_w = midpos;
+		pos_left_w = midpos; // pos_left_w will return as a reference the position of the object found
 		return true;
 	}
-
-
 }
 
 }
@@ -63,12 +58,9 @@ if( window_length == 0 )
 
 
 
-// Same function but with a counter of interations
+// Same solution but with a counter of iterations
 template<typename T>
 bool binary_search_research(const std::vector<T>& v, const T& thing2search, int& window_length, int& pos_left_w, int& it_count){
-
-it_count++;
-
 
 if( window_length == 0 )
 {
@@ -76,39 +68,20 @@ if( window_length == 0 )
 
 }else{
 
+
+	it_count++;
 	int midpos = pos_left_w + (window_length)/2;
-
-/*
- * 	int rest = window_length % 2;
- *
-	for(int i=pos_left_w; i< pos_left_w + window_length; i++)
-	{
-		std::cout << v[i] << "["  << i << "],";
-
-	}
-	std::cout << std::endl;
-	std::cout << v[midpos] << "["  << midpos << "]" << std::endl;
-*/
 
 	if( thing2search < v[midpos]) // Is on the left side:
 	{
 
-		window_length = window_length/2; // new window length
-		// Next position (left of the window)
-		// pos_left_w is tha same than before
-
-		//std::cout << rest << "[rest]. Go to left, " << v[pos_left_w] << "["<<pos_left_w<<"], with input window length: " << window_length << std::endl;
+		window_length = window_length/2;
 		return binary_search_research(v, thing2search, window_length, pos_left_w, it_count);
 
-	}else if (thing2search > v[midpos]){ // It is on the rigth side:
+	}else if (thing2search > v[midpos]){ // It is on the right side:
 
-		// when window_length is even number, the window to the right side, must be 1 less position
-		window_length = (window_length-1)/2;
-
-		// Next position (left of the window)
+		window_length = (window_length - 1)/2;
 		pos_left_w = midpos+1;
-
-		//std::cout << rest << "[rest]. Go to right, " << v[pos_left_w] << "["<<pos_left_w<<"], with input window length: " << window_length << std::endl;
 		return binary_search_research(v, thing2search, window_length, pos_left_w, it_count);
 
 	}else{
@@ -123,8 +96,9 @@ if( window_length == 0 )
 }
 
 
-
-// returns 1 when the test succeed
+// param1: vector of integers ordered numbers
+// param2: number to search in into the vector
+// returns 1 when the test succeed, and 0 when fails. binary_search_researchis tested against std::find
 int run_test(const std::vector<int> v, int number2search){
 
 	
@@ -166,61 +140,56 @@ int run_test(const std::vector<int> v, int number2search){
 		}
 	}
 
-
 }
 //                 .         .
 //-------------------------------------
 
 int main(){
 
-
-	int totaltest=0;
-	int testsucc=0;
-
+	int totaltest = 0;
+	int testsucc = 0;
 	srand( (int)time(0));
-
 	const int MAX_LEN = 1 + (rand() % 100);
 	const int RND_RANGE = MAX_LEN/5;
-
 	std::vector<int> v;
+
 	// Creates a vector of ordered numbers, separated by a random distance with average RND_RANGE
 	for(int i = 0; i < MAX_LEN; i++){
 
 		int number = i*RND_RANGE + (rand() % RND_RANGE);
-		v.push_back( number );
+		v.push_back(number);
 
 		if(i < MAX_LEN-1){
 			std::cout << number << "["  << i << "]" << ",";
 		}else{
 			std::cout << number<< "["  << i << "]" << std::endl;
 		}
-
 	}
-
 	std::cout << std::endl;
+
 	int number2search = v[0];
-
-	testsucc=testsucc+run_test(v, number2search);
+	testsucc = testsucc + run_test(v, number2search);
 	totaltest++;
+
 	number2search = v[static_cast<int>(v.size())-1];
-	testsucc=testsucc+run_test(v, number2search);
+	testsucc = testsucc + run_test(v, number2search);
 	totaltest++;
 
 	std::cout << std::endl;
-	number2search = v[0]-1;
-
-	testsucc=testsucc+run_test(v, number2search);
+	number2search = v[0] - 1;
+	testsucc = testsucc + run_test(v, number2search);
 	totaltest++;
+
 	number2search = v[static_cast<int>(v.size())-1]+1;
-	testsucc=testsucc+run_test(v, number2search);
+	testsucc = testsucc + run_test(v, number2search);
 	totaltest++;
 
 	for(int i=1; i< 100 ; i++){
 
-	    srand( (int)time(0)+i);
+	    srand( (int)time(0) + i );
 
 		std::vector<int> v;
-		// Creates a vector of ordered numbers, separated by a random distance with average RND_RANGE
+		// Creates a vector of ordered numbers, separated by a random distance average RND_RANGE
 		for(int i = 0; i < MAX_LEN; i++){
 
 			int number = i*RND_RANGE + (rand() % RND_RANGE);
@@ -231,13 +200,12 @@ int main(){
 			}else{
 				std::cout << number<< "["  << i << "]" << std::endl;
 			}
-
 		}
 
 		std::cout << std::endl;
 		int number2search = (rand() % (MAX_LEN*RND_RANGE));
 
-		testsucc=testsucc+run_test(v, number2search);
+		testsucc = testsucc + run_test(v, number2search);
 		totaltest++;
 		std::this_thread::sleep_for(std::chrono::milliseconds(300));
 	}
